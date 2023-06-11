c690f5c5b1b9bd58934ca08a3f34bf300ed397e1
Tuminha <83960151+Tuminha@users.noreply.github.com>
2023-06-11 20:32:27 +0200

    Updated tokens

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -30,7 +30,7 @@ export const makeChain = (vectorstore: PineconeStore) => {
   const model = new OpenAI({
     temperature: 0.4, // increase temepreature to get more creative answers
     modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
-    maxTokens: 3000, // for instance
+    maxTokens: 2500, // for instance
   });
 
   // Create a chain that can be used to answer questions
