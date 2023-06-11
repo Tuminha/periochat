d2a22b8820022e8ce3bd75d97716f1651e76d18f
Tuminha <83960151+Tuminha@users.noreply.github.com>
2023-06-11 12:23:38 +0200

    Updated the Nav

--- components/layout.tsx
+++ components/layout.tsx
@@ -5,10 +5,12 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="mx-auto flex flex-col space-y-4">
-      <header className="container sticky top-0 z-40 bg-white">
+      {/* Updated header */}
+      <header className="container sticky top-0 z-40 bg-black">
         <div className="h-16 border-b border-b-slate-200 py-4">
           <nav className="ml-4 pl-6">
-            <a href="#" className="hover:text-slate-600 cursor-pointer">
+            {/* Updated "Home" button */}
+            <a href="#" className="hover:text-white cursor-pointer">
               Home
             </a>
           </nav>
